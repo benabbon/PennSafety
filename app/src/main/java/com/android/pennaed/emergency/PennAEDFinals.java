1e0c1dc7c24fea6385ddc615cf13891c87a4be77
Nabil BENABBOU <nabil.benabbou@ensimag.grenoble-inp.fr>
2014-11-01 18:32:04 -0400

    Notification with basic setting activity

--- app/src/main/java/com/android/pennaed/emergency/PennAEDFinals.java
+++ app/src/main/java/com/android/pennaed/emergency/PennAEDFinals.java
@@ -1,5 +1,5 @@
 package com.android.pennaed.emergency;
 
 public class PennAEDFinals {
-	public static final String EMERGENCY_PHONE_NUMBER = "tel:" + "9145759309";
+	public static final String EMERGENCY_PHONE_NUMBER = "tel:" + "0000000000";
 }
---
+++ app/src/main/java/com/android/pennaed/outOfReach/Notification.java
@@ -0,0 +1,54 @@
+package com.android.pennaed.outOfReach;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+
+import com.android.pennaed.MainNavigation;
+import com.android.pennaed.R;
+
+/**
+ * Created by nabilbenabbou1 on 11/1/14.
+ */
+public class Notification {
+
+	public static void createOutOfReachNotification(Activity activity) {
+
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(activity);
+		if (!preferences.getBoolean("allow_notifications", true)) {
+			return;
+		}
+
+		NotificationCompat.Builder mBuilder =
+				new NotificationCompat.Builder(activity)
+						.setSmallIcon(R.drawable.penn_logo)
+						.setContentTitle("Out of reach")
+						.setContentText("You're now in a zone where penn safety can't reach you.");
+		// Specifiy the call back when clicking on the notification.
+		Intent resultIntent = new Intent(activity, MainNavigation.class);
+
+		// The stack builder object will contain an artificial back stack for the
+		// started Activity.
+		// This ensures that navigating backward from the Activity leads out of
+		// your application to the Home screen.
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(activity);
+		// Adds the Intent that starts the Activity to the top of the stack
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent =
+				stackBuilder.getPendingIntent(
+						0,
+						PendingIntent.FLAG_UPDATE_CURRENT
+				);
+		mBuilder.setContentIntent(resultPendingIntent);
+		NotificationManager mNotificationManager =
+				(NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
+		// id allows to update the notification later on.
+		mNotificationManager.notify(1, mBuilder.build());
+	}
+}
