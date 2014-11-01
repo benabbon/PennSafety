d2a4c11ac4e133dedb8f71b1444e8d0a41a6e1af
Nabil BENABBOU <nabil.benabbou@ensimag.grenoble-inp.fr>
2014-11-01 19:03:30 -0400

    Settings in the action bar

--- app/src/main/java/com/android/pennaed/MainNavigation.java
+++ app/src/main/java/com/android/pennaed/MainNavigation.java
@@ -3,12 +3,17 @@ package com.android.pennaed;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.android.pennaed.contacts.ContactsFragment;
 import com.android.pennaed.emergency.EmergencyFragment;
+import com.android.pennaed.outOfReach.Notification;
+import com.android.pennaed.outOfReach.SettingsActivity;
 
 
 public class MainNavigation extends Activity
@@ -37,6 +42,19 @@ public class MainNavigation extends Activity
 		mNavigationDrawerFragment.setUp(
 				R.id.navigation_drawer,
 				(DrawerLayout) findViewById(R.id.drawer_layout));
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+			case R.id.action_settings:
+				Intent i = new Intent(this, SettingsActivity.class);
+				startActivity(i);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 
 	@Override
@@ -80,6 +98,8 @@ public class MainNavigation extends Activity
 			// if the drawer is not showing. Otherwise, let the drawer
 			// decide what to show in the action bar.
 			restoreActionBar();
+			MenuInflater inflater = getMenuInflater();
+			inflater.inflate(R.menu.global, menu);
 			return true;
 		}
 		return super.onCreateOptionsMenu(menu);
