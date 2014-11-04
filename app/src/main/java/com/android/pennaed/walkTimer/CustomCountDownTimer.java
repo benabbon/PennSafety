7a20b1f1b6da98bd98dfd0a4a7174ce28a811205
ssn2013 <ssn2009@gmail.com>
2014-11-04 15:12:24 -0500

    all changes

--- app/src/main/java/com/android/pennaed/walkTimer/CustomCountDownTimer.java
+++ app/src/main/java/com/android/pennaed/walkTimer/CustomCountDownTimer.java
@@ -18,41 +18,43 @@ public class CustomCountDownTimer{
 	private WalkTimerMapActivity parentActivity;
 	private long timeInMs;
 
-	public CustomCountDownTimer() {
-		countDownTimer =  new android.os.CountDownTimer(30000, 1000) {
-
-			public void onTick(long millisUntilFinished) {
-				onTick(millisUntilFinished);
-			}
-
-			public void onFinish() {
-				onFinish();
-			}
-		}.start();
-	}
-
 	public CustomCountDownTimer(WalkTimerMapActivity parentActivity) {
 		this.parentActivity = parentActivity;
+		counterState = CounterState.STOPPED;
 	}
 
-	public void onFinish() {
+	public void onTimerFinish() {
 		Toast.makeText(parentActivity.getApplicationContext(), "End of timer", Toast.LENGTH_SHORT).show();
 		parentActivity.changeTimerButtonText("Timer ended");
 		counterState = CounterState.STOPPED;
 		countDownTimer.cancel();
 	}
 
-	public void onTick(long millisUntilFinished) {
+	public void onTimerTick(long millisUntilFinished) {
 		long secs = millisUntilFinished / 1000;
 		parentActivity.changeTimerButtonText(secs / 60 + "m " + (secs % 60) + "s");
 	}
 
 	public void startTimer(long timeInMs) {
 		this.timeInMs = timeInMs;
-		startTimer(this.timeInMs);
+		counterState = CounterState.RUNNING;
+		countDownTimer = new CountDownTimer(timeInMs, 1000) {
+			@Override
+			public void onTick(long millisUntilFinished) {
+				onTimerTick(millisUntilFinished);
+			}
+
+			@Override
+			public void onFinish() {
+				onTimerFinish();
+			}
+		}.start();
 	}
 
 	public void stopTimer() {
+		if(counterState == CounterState.STOPPED) {
+			return;
+		}
 		counterState = CounterState.STOPPED;
 		countDownTimer.cancel();
 	}
