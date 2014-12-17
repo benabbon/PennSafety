819f312aa0f729f363b7764455886c90df361856
chaitalisg <chaitaligondhalekar@gmail.com>
2014-12-16 20:51:25 -0500

    instruction video tests

--- app/src/androidTest/java/com/android/pennaed/test/instructionVideo/InstructionVideoTest.java
+++ app/src/androidTest/java/com/android/pennaed/test/instructionVideo/InstructionVideoTest.java
@@ -1,4 +1,4 @@
-package com.android.pennaed.InstructionVideo;
+package com.android.pennaed.test.instructionVideo;
 
 import android.content.Intent;
 import android.test.AndroidTestCase;
@@ -7,13 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.pennaed.InstructionVideo.InstructionVideo;
 import com.android.pennaed.R;
 import com.android.pennaed.outOfReach.LocationReceiver;
 import com.google.android.gms.location.LocationClient;
+
+import junit.framework.TestCase;
+
 /**
  * Created by anand on 12/16/14.
  */
-public class InstructionVideoTest extends AndroidTestCase {
+public class InstructionVideoTest extends TestCase {
     private InstructionVideo video;
     @Override
     protected void setUp() throws Exception
